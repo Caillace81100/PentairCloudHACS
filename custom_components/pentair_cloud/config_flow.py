b9e481684d9caf893956804bf2486d792dc6bc66
Caillace81100 <b.philippot81@gmail.com>
2025-07-25 17:47:31 +0200

    Premiere modification

--- custom_components/pentair_cloud/config_flow.py
+++ custom_components/pentair_cloud/config_flow.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 
 import logging
+import asyncio
 from typing import Any
 
 from pypentair import Pentair, PentairAuthenticationError
@@ -10,13 +11,44 @@ import voluptuous as vol
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.core import HomeAssistant
+from .pentaircloud import PentairCloudHub
 
 from .const import DOMAIN
+
 
 _LOGGER = logging.getLogger(__name__)
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {vol.Required(CONF_USERNAME): str, vol.Required(CONF_PASSWORD): str}
 )
+
+async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+    # TODO validate the data can be used to set up a connection.
+
+    # If your PyPI package is not built with async, pass your methods
+    # to the executor:
+    # await hass.async_add_executor_job(
+    #     your_validate_func, data["username"], data["password"]
+    # )
+
+    hub = PentairCloudHub(_LOGGER)
+    if not await hass.async_add_executor_job(
+        hub.authenticate, data["username"], data["password"]
+    ):
+        raise InvalidAuth
+
+    # If you cannot connect:
+    # throw CannotConnect
+    # If the authentication is wrong:
+    # InvalidAuth
+
+    # Return info that you want to store in the config entry.
+    return {"title": "PentairCloudHACS"}
+
 
 
 class PentairConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -115,3 +147,36 @@ class PentairConfigFlow(ConfigFlow, domain=DOMAIN):
         return await self.async_pentair_login(
             step_id="reauth_confirm", user_input=user_input, schema=reauth_schema
         )
+
+    async def async_step_user(self, user_input: dict[str, Any] | None = None ) -> FlowResult:
+        """Handle the initial step."""
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user", data_schema=STEP_USER_DATA_SCHEMA
+            )
+
+        errors = {}
+
+        try:
+            info = await validate_input(self.hass, user_input)
+        except CannotConnect:
+            errors["base"] = "cannot_connect"
+        except InvalidAuth:
+            errors["base"] = "invalid_auth"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
+        else:
+            return self.async_create_entry(title=info["title"], data=user_input)
+
+        return self.async_show_form(
+            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+        )
+
+
+class CannotConnect(HomeAssistantError):
+    """Error to indicate we cannot connect."""
+
+
+class InvalidAuth(HomeAssistantError):
+    """Error to indicate there is invalid auth."""
